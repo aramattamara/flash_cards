abc28d45b4fc5bd200c35865dbed76b27e95b9d1
Tamara <toma.vashchuk@gmail.com>
2023-07-18 16:27:50 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './index.css';
+import {useState} from "react";
 
-function App() {
+export default function App(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <FlashCards/>
+      </div>
   );
 }
 
-export default App;
+const words = [
+  {
+    id: 1,
+    word: "kickstart",
+    definition: "to make functional, productive again"
+  },
+  {
+    id: 2,
+    word: "soar",
+    definition: "to rise, increase in size or volume"
+  },
+  {
+    id: 3,
+    word: "slump",
+    definition: "to sink or fall heavily"
+  },
+  {
+    id: 4,
+    word: "cash flow",
+    definition: "the movement of money in and out of business"
+  },
+  {
+    id: 5,
+    word: "to yield",
+    definition: "to concede, to repay"
+  },
+  {
+    id: 6,
+    word: "option",
+    definition: "something that offers the buyer the right, but not the obligation, to buy or sell an asset at an agreed-upon pricce at a specific time"
+  },
+  {
+    id: 7,
+    word: "industry cluster",
+    definition: "a group of similar companies situated close to each other"
+  },
+  {
+    id: 8,
+    word: "niche",
+    definition: "a position suitable for occupying"
+  },
+]
+
+function FlashCards(){
+  const [selectedId, setSelectedId] = useState(8)
+
+  function handleClick(id) {
+    setSelectedId(id !== selectedId ? id : null)
+  }
+
+  return <div className="flashcards">
+    {words.map(word =>
+        <div
+            key={words.id}
+            onClick={()=>handleClick(word.id)}
+            className={word.id === selectedId ? "selected" : ""}
+        >
+          <p>{word.id === selectedId ? word.word : word.definition}</p>
+        </div>)}
+  </div>
+}
