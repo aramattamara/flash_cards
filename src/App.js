e41839bf2aa4827d345db497e98251eb11f48961
Tamara <toma.vashchuk@gmail.com>
2023-07-19 09:59:58 -0700

    Lifting Up State onAddPairs

--- src/App.js
+++ src/App.js
@@ -2,14 +2,6 @@ import React from 'react';
 import './index.css';
 import {useState} from "react";
 
-export default function App(){
-  return (
-      <div className="App">
-        <FlashCards/>
-      </div>
-  );
-}
-
 const words = [
   {
     id: 1,
@@ -53,7 +45,57 @@ const words = [
   },
 ]
 
-function FlashCards(){
+export default function App(){
+  const[pairs, setPairs] = useState([])
+
+  function handleAddPairs(pair){
+    setPairs(pairs=> [...pairs, pair])
+  }
+  return (
+      <div className="App">
+        <Form onAddPairs={handleAddPairs}/>
+        <FlashCards pairs={pairs}/>
+      </div>
+  );
+}
+
+function Form ({onAddPairs}){
+  const[word, setWord] = useState()
+  const[definition, setDefinition] = useState()
+
+  function handleSubmit(e){
+    if (!definition) return;
+    e.preventDefault();
+
+    const newPair = {word, definition, id: Date.now()};
+    console.log(newPair)
+
+    onAddPairs(newPair)
+
+    setWord("")
+    setDefinition("")
+
+  }
+
+  return <form onSubmit={handleSubmit}>
+    <h3>Would you like to add new words?</h3>
+    <input
+        type="text"
+        placeholder="word"
+        value={word}
+        onChange={(e)=> setWord(e.target.value)}
+    />
+    <input
+        type="text"
+        placeholder="desription"
+        value={definition}
+        onChange={(e)=>setDefinition(e.target.value)}
+    />
+    <button>Add</button>
+  </form>
+}
+
+function FlashCards({pairs}){
   const [selectedId, setSelectedId] = useState(8)
 
   function handleClick(id) {
@@ -61,13 +103,13 @@ function FlashCards(){
   }
 
   return <div className="flashcards">
-    {words.map(word =>
+    {pairs.map(pair =>
         <div
-            key={words.id}
-            onClick={()=>handleClick(word.id)}
-            className={word.id === selectedId ? "selected" : ""}
+            key={pair.id}
+            onClick={()=>handleClick(pair.id)}
+            className={pair.id === selectedId ? "selected" : ""}
         >
-          <p>{word.id === selectedId ? word.word : word.definition}</p>
+          <p>{pair.id === selectedId ? pair.word : pair.definition}</p>
         </div>)}
   </div>
 }
